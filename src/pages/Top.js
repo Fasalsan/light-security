b232dcc43e2b61febc0eeb770c83d9fe3d48008d
Fasalsan <Sanfasal70@gmail.com>
2025-06-26 16:06:30 +0700

    last fix

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,6 +1,5 @@
 import React from "react";
-import IMG_009 from "../assets/maxresdefault.jpg"; // Adjust the path as necessary
-
+import IMG_009 from "../assets/maxresdefault.jpg"; 
 export default function Top() {
     return (
         <div className="relative w-full h-80 sm:h-60 md:h-80 lg:h-96 overflow-hidden">
@@ -10,11 +9,7 @@ export default function Top() {
                 className="w-full h-full object-cover"
             />
 
-            {/* Overlay with blur and dark background */}
             <div className="absolute inset-0 bg-black/30  flex items-end justify-center">
-                {/* <h1 className="text-white text-2xl sm:text-4xl font-bold drop-shadow-lg bg-amber-300 w-full text-center ">
-          Welcome to Our Website
-        </h1> */}
 
                 <div className="overflow-hidden bg-red-900 w-full">
                     <div
